5e38d3b245dee4dd46cd7006dfa91d9c1fcd1c89
Peter Aleksander Bizjak <peteralex.bizjak@gmail.com>
2019-11-25 12:00:45 +0100

    Minor modifications

--- src/02-classes/openlab/__init__.py
+++ src/02-classes/openlab/__init__.py
@@ -3,3 +3,12 @@ class OpenLab:
         self.mentor_list = mentor_list
         self.workshop_list = workshops_list
         self.number_of_employees = number_of_employees
+
+    def mentors_count(self):
+        print('OpenLab has ' + str(len(self.mentor_list)) + ' mentors')
+
+    def workshops(self):
+        print('We offer ' + str(len(self.workshop_list)) + ' workshops: ' + str(', '.join(self.workshop_list)))
+
+    def total_number_of_employees(self):
+        print('OpenLab has a team of ' + str(self.number_of_employees) + ' people!')
